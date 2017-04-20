439f23f309e33faed28760950d7ccf1f7cbfc1ee
William Norman <lordcrekit@gmail.com>
2017-04-20 14:55:19 -0400

    fixed jokes

--- index.js
+++ index.js
@@ -14,19 +14,24 @@ app.get('/', function (req, res) {
   res.send('<h1>hey xD</h1>')
 })
 
-var jokes = ['CSS', 'The english language', 'Donald Trump']
-app.get('/random-joke', function (req, res) {
-  res.render('joke.ejs', {'joke': jokes[Math.floor(Math.random() * jokes.length)]})
-})
+var jokes = [
+  'Knock, knock!<br/>Who’s there?<br/>Gorilla<br/>Gorilla who?<br/>Gorilla me a burger or three!',
+  'Knock, knock!<br/>Who’s there?<br/>Opportunity!<br/>That is impossible. Opportunity doesn’t come knocking twice!', 
+  'Knock knock.<br/>Who’s there?<br/>An extraterrestrial.<br/>Extraterrestrial who?<br/>What – how many extra-terrestrials do you know?',
+  'Knock knock.<br/>Who’s there?<br/>Beats.<br/>Beats who?<br/>Beats me.',
+'Knock knock.<br/>Who’s there?<br/>The interrupting cow.<br/>Interrupting cow wh-<h2>Moooooo!</h2>' ]
+  app.get('/random-joke', function (req, res) {
+    res.render('joke.ejs', {'joke': jokes[Math.floor(Math.random() * jokes.length)]})
+  })
 
-app.get('/cuteness', function (req, res) {
-  res.render('cat.ejs', {})
-})
+  app.get('/cuteness', function (req, res) {
+    res.render('cat.ejs', {})
+  })
 
-app.get('*', function (req, res) {
-  res.render('404.ejs', {'url': req.url})
-})
+  app.get('*', function (req, res) {
+    res.render('404.ejs', {'url': req.url})
+  })
 
-app.listen(app.get('port'), function () {
-  console.log('Node app is running on port', app.get('port'))
-})
+  app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'))
+  })
